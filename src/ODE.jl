bd652323a892270927e7cbf3b5bc8bebe1fb13bc
Jorge Gidi <jorgegidi@gmail.com>
2022-04-26 12:12:56 -0400

    export rungekutta10_4

--- src/ODE.jl
+++ src/ODE.jl
@@ -5,7 +5,7 @@ using FFTW, ProgressMeter
 
 # The objects 'exported' here become available for the user
 # after importing this package
-export integrate_ode, derivative, rungekutta4, euler
+export integrate_ode, derivative, rungekutta4, rungekutta10_4, euler
 
 # Its awkward to have everything in the same file.
 # Lets split the package into multiple files!
